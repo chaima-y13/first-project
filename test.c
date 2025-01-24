662d7373406d5447514abc615fe188da606f8948
chaima-y13 <kacemchaima13@gmail.com>
2025-01-24 22:55:46 +0100

    modified two files

--- test.c
+++ test.c
@@ -1,4 +1,8 @@
 #include<stdio.h>
-int main (){
-    printf("hi am chaima");
+int main (){ 
+    char nom[35];
+    printf("hi am chaima\n");
+ printf("donner votre nom\t");
+ scanf("%s",nom);
+ printf("a bon votre est %s",nom);
 }
